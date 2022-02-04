a68f8298cc55b41661dbc1afb275685bf856a885
harshwardhan <hratore076@gmail.com>
2022-02-04 11:10:54 +0530

    database update

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from flask_gravatar import Gravatar
 import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['SECRET_KEY'] = 'fsahfhlsbanfherio1r7eyhro1738eyrh'
 ckeditor = CKEditor(app)
 Bootstrap(app)
 gravatar = Gravatar(app, size=100, rating='g', default='retro', force_default=False, force_lower=False, use_ssl=False, base_url=None)
@@ -190,6 +190,7 @@ def delete_post(post_id):
     db.session.delete(post_to_delete)
     db.session.commit()
     return redirect(url_for('get_all_posts', current_user=current_user))
+
 
 @app.route('/logout')
 def logout():
